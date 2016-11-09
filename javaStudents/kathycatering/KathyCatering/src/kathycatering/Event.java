934fe0d9aee18d179fb668aa2cd73c57d080ecd0
TreTKE <tre.watts@thyssenkrupp.com>
2016-11-09 07:27:50 -0500

    finished kathy's catering project (i think)

--- javaStudents/kathycatering/KathyCatering/src/kathycatering/Event.java
+++ javaStudents/kathycatering/KathyCatering/src/kathycatering/Event.java
@@ -6,6 +6,7 @@
 package kathycatering;
 
 import java.util.Date;
+import java.util.Random;
 
 /**
  *
@@ -16,15 +17,76 @@ public class Event {
     private Date dateOf;
     private int numGuest;
     private double eventCost = 40;
+    //private Random randnum = new Random();
     private String eventNum;
+    static int eventNumStem = 0;
     
-    public Event(String eventType, Date dateOf, int numGuest, String eventNum) {
+    ///
+    /// Create different constructors
+    ///
+    
+    // ..for when event type, date, and number of guests are all known
+    public Event(String eventType, Date dateOf, int numGuest) {
         this.eventType = eventType;
         this.dateOf = dateOf;
         this.numGuest = numGuest;
-        this.eventNum = eventNum; //event number
-        //this.eventCost = eventCost;
+        
+        if (eventNumStem == 0){
+            Random randnum = new Random();
+            eventNumStem = randnum.nextInt();
+        } else {
+            eventNumStem++;
+        }
+        char firstChar = this.eventType.charAt(0);
+        this.eventNum = Character.toString(firstChar) + Integer.toString(eventNumStem);
     }
+    
+    //...for when only event type and date are known
+    public Event(String eventType, Date dateOf) {
+        this.eventType = eventType;
+        this.dateOf = dateOf;
+        
+        if (eventNumStem == 0){
+            Random randnum = new Random();
+            eventNumStem = randnum.nextInt();
+        } else {
+            eventNumStem++;
+        }
+        char firstChar = this.eventType.charAt(0);
+        this.eventNum = Character.toString(firstChar) + Integer.toString(eventNumStem);//.toString();
+    }
+    
+    //...for when only the event type is known
+    public Event(String eventType) {
+        this.eventType = eventType;
+        
+        if (eventNumStem == 0){
+            Random randnum = new Random();
+            eventNumStem = randnum.nextInt();
+        } else {
+            eventNumStem++;
+        }
+        char firstChar = this.eventType.charAt(0);
+        this.eventNum = Character.toString(firstChar) + Integer.toString(eventNumStem);
+    }
+    
+    //Special Methods
+    
+    public double calculateTotal() {
+        return this.numGuest * this.eventCost;
+    }
+    
+    public void printEvent() {
+        System.out.println("Event Number: " + eventNum);
+        System.out.println("Event Type: " + eventType);
+        System.out.println("Date: " + this.getEventDate());
+        System.out.println("Number of Guests: " + this.numGuest);
+        System.out.println("Total Cost: " + this.calculateTotal());
+        System.out.println("-------------------------------------");
+    }
+    //////
+    //Setter methods
+    /////
     
     public void setEventType(String eventType) {
         this.eventType = eventType;
@@ -46,15 +108,22 @@ public class Event {
         this.eventNum = eventNum;
     }
     
+    //
+    //Getter methods
+    //
+    
     public String getEventType() {
         return this.eventType;
     }
+    
     public Date getEventDate() {
         return this.dateOf;
     }
+    
     public double getEventCost() {
         return this.eventCost;
     }
+    
     public String getEventNumber() {
         return this.eventNum;
     }
--- javaStudents/kathycatering/KathyCatering/src/kathycatering/KathyCatering.java
+++ javaStudents/kathycatering/KathyCatering/src/kathycatering/KathyCatering.java
@@ -5,6 +5,8 @@
  */
 package kathycatering;
 
+import java.util.Date;
+
 /**
  *
  * @author Gertrude
@@ -16,6 +18,20 @@ public class KathyCatering {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Date dateOf = new Date();
+        dateOf.setMonth(9);
+        dateOf.setDate(9);
+        dateOf.setYear(2001-1900);
+        
+        String eventType = "wedding";
+        int numGuests = 30;
+        
+        System.out.println(dateOf.toString());
+        Event wed1 = new Event(eventType, dateOf, numGuests);
+        Event wed2 = new Event("birthday", dateOf, numGuests + 3);
+        
+        wed1.printEvent();
+        wed2.printEvent();
     }
     
 }
